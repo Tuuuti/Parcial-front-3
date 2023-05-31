58149dfde78dacd21d52127cb152f46fe432b08d
Tuuuti <agussoto10@gmail.com>
2023-05-31 19:08:39 -0300

    --PreParcial

---
+++ Template_parcial/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Card from './Card'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>Carga de estudiantes</h1>
+    <form></form>
+      <Card></Card>
+    </>
+  )
+}
+
+export default App
